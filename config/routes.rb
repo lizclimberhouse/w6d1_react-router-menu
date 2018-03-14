1bf7421e5b3cf860633b0bdf70acd71f7dce62f1
Liz House <lizehouse@gmail.com>
2018-03-13 21:00:52 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  namespace :api do
+    resources :items
+    #API ROUTES SHOULD GO HERE
+  end
+
+  #Do not place any routes below this one
+  get '*other', to: 'static#index'
+end
